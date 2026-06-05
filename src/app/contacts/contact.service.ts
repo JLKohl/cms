a31e7d983eb605d4b0ee7f332efc5a70b197b5d9
JLKohl <121449936+JLKohl@users.noreply.github.com>
2026-06-05 15:03:53 -0500

    finshed with docmuent service

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -10,12 +10,14 @@ import { MOCKCONTACTS } from './MOCKCONTACTS';
 })
 export class ContactService {
   
-  contacts: Contact[] = [];
+  contacts: Contact[] = MOCKCONTACTS;
+
+  maxContactId: number;
 
   contactChangedEvent = new Subject<Contact[]>();
 
   constructor() {
-    this.contacts = MOCKCONTACTS;
+    this.maxContactId = this.getMaxId();
   }
 
   getContacts(): Contact[] {
@@ -26,6 +28,43 @@ export class ContactService {
     return this.contacts.find(contact => contact.id === id)!;
   }
 
+  getMaxId(): number {
+    let maxId = 0;
+  
+    for (const contact of this.contacts) {
+      const currentId = +contact.id;
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+  
+    return maxId;
+  }
+
+  addContact(newContact: Contact) {
+    if (!newContact) return;
+  
+    this.maxContactId++;
+    newContact.id = this.maxContactId.toString();
+  
+    this.contacts.push(newContact);
+  
+    this.contactChangedEvent.next(this.contacts.slice());
+  }
+  
+  updateContact(originalContact: Contact, newContact: Contact) {
+    if (!originalContact || !newContact) return;
+  
+    const pos = this.contacts.indexOf(originalContact);
+    if (pos < 0) return;
+  
+    newContact.id = originalContact.id;
+  
+    this.contacts[pos] = newContact;
+  
+    this.contactChangedEvent.next(this.contacts.slice());
+  }
+
   deleteContact(contact: Contact) {
     if (!contact) return;
   
@@ -33,7 +72,7 @@ export class ContactService {
     if (pos < 0) return;
   
     this.contacts.splice(pos, 1);
+  
     this.contactChangedEvent.next(this.contacts.slice());
-  }
-
+  } 
 }
